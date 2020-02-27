adc4376825f05d724d9ec2834a4f04db602f3abb
unknown <demophp0205chat>
2020-02-27 16:47:19 +0700

    change mail

--- shoppage/shoppage/settings.py
+++ shoppage/shoppage/settings.py
@@ -156,7 +156,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bluenight0104@gmail.com'
+EMAIL_HOST_USER = 'bluenight0105@gmail.com'
 EMAIL_HOST_PASSWORD = 'latelyyou'
 EMAIL_USE_TLS = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'

44685db9a1e3c0f1370e3ac522b2f3b094bd084a
unknown <demophp0205chat>
2020-02-28 08:28:54 +0700

    add more css

--- shoppage/shoppage/settings.py
+++ shoppage/shoppage/settings.py
@@ -84,35 +84,35 @@ WSGI_APPLICATION = 'shoppage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-            'default': {
-                        'ENGINE': 'django.db.backends.mysql',
-                        'OPTIONS': {
-                                    'read_default_file': '/path/to/my.cnf',
-                                    },
-                        'NAME': 'khoaphamdev$shoppage',
-                        'USER': 'khoaphamdev',
-                        'PASSWORD': 'shop_page',
-                        'HOST': 'khoaphamdev.mysql.pythonanywhere-services.com',   # Or an IP Address that your DB is hosted on
-                        }
-            }
-
-# ???
-#
 # DATABASES = {
 #             'default': {
 #                         'ENGINE': 'django.db.backends.mysql',
 #                         'OPTIONS': {
 #                                     'read_default_file': '/path/to/my.cnf',
 #                                     },
-#                         #   'NAME': 'shop_page_update_customer',
-#                         'NAME': 'shop_page',
-#                         'USER': 'root',
-#                         'PASSWORD': '',
-#                         'HOST': '',   # Or an IP Address that your DB is hosted on
-#                         'PORT': '3306',
+#                         'NAME': 'khoaphamdev$shoppage',
+#                         'USER': 'khoaphamdev',
+#                         'PASSWORD': 'shop_page',
+#                         'HOST': 'khoaphamdev.mysql.pythonanywhere-services.com',   # Or an IP Address that your DB is hosted on
 #                         }
 #             }
+
+
+#
+DATABASES = {
+            'default': {
+                        'ENGINE': 'django.db.backends.mysql',
+                        'OPTIONS': {
+                                    'read_default_file': '/path/to/my.cnf',
+                                    },
+                        #   'NAME': 'shop_page_update_customer',
+                        'NAME': 'shop_page',
+                        'USER': 'root',
+                        'PASSWORD': '',
+                        'HOST': '',   # Or an IP Address that your DB is hosted on
+                        'PORT': '3306',
+                        }
+            }
 
 
 
